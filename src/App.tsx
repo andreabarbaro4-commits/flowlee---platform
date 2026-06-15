143d8c30277b0cba39ac5872d6b6748fd2a76396
IFTS50 <IFTS50@ideamicis.it>
2026-06-15 17:36:23 +0200

    feat: setup iniziale progetto Flowlee

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,161 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import heroImg from './assets/hero.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState<number>(1);
+  const [userType, setUserType] = useState<'company' | 'solo' | null>(null);
+
+  // Stati per gestire i campi del form
+  const [companyName, setCompanyName] = useState<string>('');
+  const [teamSize, setTeamSize] = useState<string>('30-50');
+  const [description, setDescription] = useState<string>(
+    
+  );
+
+  const handleSelectType = (type: 'company' | 'solo') => {
+    setUserType(type);
+  
+    if (type === 'solo') {
+      setCompanyName('Freelance / Nome');
+      setTeamSize('1-n10');
+    }
+      setCompanyName('Freelance / Nome');
+    setStep(2);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="app-wrapper">
+     
+      <div className={`blob-bg ${step === 2 ? 'step-2-active' : ''}`}></div>
 
-      <div className="ticks"></div>
+      <main className="app-container">
+        
+        {step === 1 && (
+          <section className="card-layout step-one animate-fade-in">
+            <header className="card-header">
+              <span className="breadcrumb">Organizzazione / 01</span>
+              <div className="brand-header">
+                <span className="brand-text">Flowlee</span>
+              </div>
+              <span className="user-profile">Andrea ::</span>
+            </header>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            <div className="Uno">
+              <div className="Due">
+                <h1 className="main-title">
+                  Iniziamo,senza perdere altro tempo. <br />
+                  Lavori con un'azienda<br />
+                  o sei un freelance?
+                  Diccelo qui sotto.
+                </h1>
+                
+                <div className="5">
+                  <button className="btn btn-dark" onClick={() => handleSelectType('company')}>
+                   Azienda
+                  </button>
+                  <button className="btn btn-light" onClick={() => handleSelectType('solo')}>
+                    Lavoro da solo
+                  </button>
+                </div>
+              </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+              <div className="content-right">
+                <div className="hero-image-wrapper">
+                  <img src={heroImg} alt="Flowlee Illustration" className="hero-image" />
+                </div>
+              </div>
+            </div>
+          </section>
+        )}
+
+        {step === 2 && (
+          <section className="card-layout step-two animate-fade-in">
+            <header className="card-header">
+              <span className="breadcrumb">Organizzazione / 02</span>
+              <div className="brand-header">
+                <span className="brand-text">Flowlee</span>
+              </div>
+              <span className="user-profile">Flavio ::</span>
+            </header>
+
+            <div className="card-body form-split">
+          
+              <div className="Tre">
+                <span className="box-label">Logo</span>
+                <div className="4">
+                  <svg className="company-logo" width="120" height="50">
+                    <use href="/icons.svg#icons" /> 
+                  </svg>
+                  <span className="logo-subtext">
+                    {userType === 'company' ? 'COMPANY' : 'SOLO'}
+                  </span>
+                </div>
+              </div>
+
+              {/* Form Input a Destra */}
+              <div className="form-container-box">
+                <form onSubmit={(e) => e.preventDefault()} className="flowlee-form">
+                  
+                  <div className="form-group">
+                    <label htmlFor="companyName">
+                      {userType === 'company' ? "Nome dell'azienda" : "Nome Professionista"}
+                    </label>
+                    <input 
+                      type="text" 
+                      id="companyName" 
+                      value={companyName} 
+                      onChange={(e) => setCompanyName(e.target.value)} 
+                    />
+                  </div>
+
+                  {/* Mostra la dimensione del team solo se è un'azienda */}
+                  {userType === 'company' && (
+                    <div className="form-group">
+                      <label htmlFor="teamSize">Numero persone</label>
+                      <div className="select-wrapper">
+                        <select 
+                          id="teamSize" 
+                          value={teamSize} 
+                          onChange={(e) => setTeamSize(e.target.value)}
+                        >
+                          <option value="1-10">1-10 persone</option>
+                          <option value="11-30">11-30 persone</option>
+                          <option value="30-50">30-55s persone</option>
+                          
+                        </select>
+                      </div>
+                    </div>
+                  )}
+
+                  <div className="form-group">
+                    <label htmlFor="description">Di cosa si occupa?</label>
+                    <textarea 
+                      id="description"
+                      rows={4}
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+
+                  
+                  <div className="form-actions">
+                    <button type="button" className="btn btn-light" onClick={() => setStep(1)}>
+                      Torna Indietro
+                    </button>
+                    <button type="submit" className="btn btn-dark">
+                      Salva e Continua
+                    </button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
